0fbf08fc017ec0de7fb77ea16073d75662ac4338
ValDNM <82450154+ValDNM@users.noreply.github.com>
2022-02-20 22:17:02 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 import java.util.*;
 
-
+// The main class to run the program
 public class Main{
 	public static void main(String[] args){
-
+			// Attributes' value are hard-coded to keep the program simple, and more focused on playing with inheritance
 			Hardware gpu1 = new Hardware(1506, 6144, "yes", 210, "PC10353060", "Zotac", "Nvidia GTX 1060 OC");
-
+			
+			// Print out functions for outputs 
 			System.out.println("Brand            : " + gpu1.getBrand());
 			System.out.println("Model            : " + gpu1.getModel());
 			System.out.println("Price            : " + gpu1.getPrice() + " USD MSRP");
