6a91e2b2db21092aff713c2ceb438b180296b078
ValDNM <82450154+ValDNM@users.noreply.github.com>
2022-02-20 22:10:46 +0700

    Update Hardware.java

--- Hardware.java
+++ Hardware.java
@@ -1,15 +1,18 @@
 public class Hardware extends Product{
 
-
+		// Hardware Class's Attributes
 		private String brand;
 		private String model;
-
+		
+		// Class's constructor
 		public Hardware(int frequency, int memorySize, String supportsCuda, int price, String idProduct, String brand, String model){
+			// Using Product class as a super in which are also the subclass of memory
 			super(frequency, memorySize, supportsCuda, price, idProduct);
 			this.brand = brand;
 			this.model = model;
 		}
-
+		
+		// Set and Get methods for each Attribute and Inheritance
 		public void setBrand(String brand){
 			this.brand = brand;
 		}
