ccd97e269cb13095e4c32b96653b637390b5959a
ValDNM <82450154+ValDNM@users.noreply.github.com>
2022-02-20 22:14:38 +0700

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,15 +1,18 @@
 public class Product extends Memory{
 
-
+	// Product Class's Attributes
 	private int price;
 	private String idProduct;
-
+	
+	// Class's constructor
 	public Product(int frequency, int memorySize, String supportsCuda, int price, String idProduct){
+		// Using Memory as a super, setting Product class as a subclass of it
 		super(frequency, memorySize, supportsCuda);
 		this.price = price;
 		this.idProduct = idProduct;
 	}
-
+	
+	// Set and Get methods for each attribute
 	public void setPrice(int price){
 		this.price = price;
 	}
